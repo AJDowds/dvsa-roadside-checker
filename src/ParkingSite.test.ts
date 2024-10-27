dfeb46904547943307dd9b3fad95185742309da6
Anthony Dowds <ajdowds89@gmail.com>
2024-10-27 22:37:59 +0000

    Add some tests for TDD

--- src/ParkingSite.test.ts
+++ src/ParkingSite.test.ts
@@ -1,12 +1,26 @@
 import { defaultBays } from "./ParkingSite.data";
 import { ParkingSite } from "./ParkingSite";
 
-test("Test Something", () => {
+test("All bays required", () => {
   const parkingSite = new ParkingSite(defaultBays);
 
-  const suitableBays = parkingSite.findContiguousBays(10);
+  const suitableBays = parkingSite.findContiguousBays(65);
 
-  console.log("!!suitableBays", suitableBays);
+  expect(suitableBays.length).toBe(1);
+});
 
-  expect(true).toBe(true);
+test("Not enough bays", () => {
+  const parkingSite = new ParkingSite(defaultBays);
+
+  const suitableBays = parkingSite.findContiguousBays(66);
+
+  expect(suitableBays.length).toBe(0);
+});
+
+test("Correct number of bays for 25m vehicle", () => {
+  const parkingSite = new ParkingSite(defaultBays);
+
+  const suitableBays = parkingSite.findContiguousBays(25);
+
+  expect(suitableBays.length).toBe(6);
 });
